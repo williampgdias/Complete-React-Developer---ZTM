799f2d4cf92aa711f8925e1cbb9620dce70f8e67
William Dias <smdwilliam89@gmail.com>
2022-05-20 23:48:43 +0100

    41. Storing Original

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: '',
     };
     console.log('constructor');
   }
@@ -32,6 +33,10 @@ class App extends Component {
   render() {
     console.log('render');
 
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className='App'>
         <input
@@ -39,18 +44,13 @@ class App extends Component {
           type='search'
           placeholder='search monsters'
           onChange={(event) => {
-            console.log(event.target.value);
-            const searchString = event.target.value.toLocaleLowerCase();
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLocaleLowerCase().includes(searchString);
-            });
-
+            const searchField = event.target.value.toLocaleLowerCase();
             this.setState(() => {
-              return {monsters: filteredMonsters};
+              return {searchField};
             });
           }}
         />
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
