cce47c669384ca893501d5b77a67deb278aad5f3
William Dias <smdwilliam89@gmail.com>
2022-05-21 12:42:12 +0100

    44. CardList Component

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,5 +1,7 @@
 import {Component} from 'react';
 
+import CardList from './components/card-list/card-list-component';
+
 import './App.css';
 
 class App extends Component {
@@ -55,13 +57,14 @@ class App extends Component {
           placeholder='search monsters'
           onChange={onSearchChange}
         />
-        {filteredMonsters.map((monster) => {
+        {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
           );
-        })}
+        })} */}
+        <CardList />
       </div>
     );
   }
